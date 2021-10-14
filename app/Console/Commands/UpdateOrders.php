9868b2ef58b2d33fdb126f3b93b039c3f92cdadd
Ravindra Singh <ravs333@gmail.com>
2021-10-14 17:45:49 +0530

    Update order status cron

--- app/Console/Commands/UpdateOrders.php
+++ app/Console/Commands/UpdateOrders.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Commands;
 
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Console\Command;
+
+use App\Models\Order;
 
 class UpdateOrders extends Command
 {
@@ -37,6 +41,26 @@ class UpdateOrders extends Command
      */
     public function handle()
     {
+        $marketplaceBaseURL = env('DESPATCH_CLOUD_MARKETPLACE_API_URL');
+        $marketplaceAPIKEY = env('DESPATCH_CLOUD_MARKETPLACE_API_KEY');
+        $apiEndpoint = env('DESPATCH_CLOUD_MARKETPLACE_ORDER_ENDPOINT');
+        
+        $pendingOrders =  Order::where('type', 'pending')->limit(50)->get()->toArray();
+
+        foreach ($pendingOrders as $pendingOrder) {
+            $apiEndpoint = env('DESPATCH_CLOUD_MARKETPLACE_ORDER_ENDPOINT') . $pendingOrder['id'];
+            $url = $marketplaceBaseURL . $apiEndpoint . '?api_key=' . $marketplaceAPIKEY;
+            $response =  Http::post($url, ['type' => 'approved'])->json();
+            Log::channel('api')->info('Get Order List API:: ', array(
+                'url' => $url,
+                'response' => $response
+            ));
+            if( isset($response['type']) && $response['type'] == 'approved' ){
+                Order::where('id', $pendingOrder['id'])
+                    ->update(['type' => 'approved']);
+            }
+        }
+
         return Command::SUCCESS;
     }
 }
